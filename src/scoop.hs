64f507e83ba733338e33f3207287af4c78881ab6
Mark Timmer <timmer@cs.utwente.nl>
2014-03-07 15:05:50 +0100

    versienummer

--- src/scoop.hs
+++ src/scoop.hs
@@ -26,7 +26,7 @@ import Data.List
 main = do
   args <- getArgs
   input <- hGetContents stdin
-  hPutStrLn stderr "SCOOP (last changed on 31 January 2014)"
+  hPutStrLn stderr "SCOOP (last changed on 07 March 2014)"
   -- Parsing the command-line parameters.
   let flags      = ["-conf", "-dead", "-mlppe", "-keeprates", "-maxprogress", "-divergence", "-ma", "-pa", "-mapa", "-imca",
                     "-checkuntil", "-cadp", "-verbose", "-trans", "-lppe", "-size", "-prism", "-aut", "-visited", "-noprob",
